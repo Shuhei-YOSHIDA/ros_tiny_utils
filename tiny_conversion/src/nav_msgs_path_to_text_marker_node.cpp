1b37dc4a77afe0fbda0fe2dcd89f21d1205c3ab7
Shuhei-YOSHIDA <s-yoshida@frontier.hokudai.ac.jp>
2021-03-07 07:14:33 +0900

    update path_to_text_marker

--- tiny_conversion/src/nav_msgs_path_to_text_marker_node.cpp
+++ tiny_conversion/src/nav_msgs_path_to_text_marker_node.cpp
@@ -1,6 +1,6 @@
 /**
  * @file nav_msgs_path_to_text_marker_node.cpp
- * @brief Subscribe a path in nav_msgs/Path, and convert it to text marker in visualizing_msgs/MarkerArray
+ * @brief Subscribe a path of nav_msgs/Path, and convert it to text marker of visualizing_msgs/MarkerArray
  */
 
 #include <ros/ros.h>
@@ -10,13 +10,49 @@
 using namespace std;
 using namespace visualization_msgs;
 using namespace nav_msgs;
+using namespace geometry_msgs;
 
 ros::Publisher marker_pub;
 ros::Subscriber path_sub;
 
+string dToStr(double value, int digit=2)
+{
+  stringstream stream;
+  stream << fixed << setprecision(digit) << value;
+  return stream.str();
+}
+
+Marker textMarker(PoseStamped poses)
+{
+  Marker m_msg;
+
+  m_msg.header = poses.header;
+  m_msg.pose = poses.pose;
+  m_msg.scale.z = 1.0;
+  m_msg.type = Marker::TEXT_VIEW_FACING;
+  m_msg.color.a = 1.0;
+  string unit = "m";
+  string delimiter = ",";
+  m_msg.text = "("
+    + dToStr(poses.pose.position.x) + unit + delimiter
+    + dToStr(poses.pose.position.y) + unit + delimiter
+    + dToStr(poses.pose.position.z) + unit + ")";
+
+  return m_msg;
+} 
+
 void pathCB(const Path::ConstPtr& msg)
 {
+  MarkerArray ma_msg;
+  for (int i = 0; i < msg->poses.size(); i++)
+  {
+    ///@Todo Convert pose by TF?
+    auto m_msg = textMarker(msg->poses[i]);
+    m_msg.id = i;
+    ma_msg.markers.push_back(m_msg);
+  }
 
+  marker_pub.publish(ma_msg);
 }
 
 int main(int argc, char** argv)
@@ -24,7 +60,7 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "path_to_text_marker_node");
   ros::NodeHandle nh;
 
-  marker_pub = nh.advertise<MarkerArray>("path_text_marker", 1);
+  marker_pub = nh.advertise<MarkerArray>("path_text_marker", 1, true);
   path_sub = nh.subscribe("path", 1, pathCB);
 
   ros::spin();
