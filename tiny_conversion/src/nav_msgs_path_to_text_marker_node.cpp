a4b291afd262ced835461b881b6274f6e186bc97
Shuhei-YOSHIDA <s-yoshida@frontier.hokudai.ac.jp>
2021-03-07 09:05:17 +0900

    TF applied for path_to_text_marker

--- tiny_conversion/src/nav_msgs_path_to_text_marker_node.cpp
+++ tiny_conversion/src/nav_msgs_path_to_text_marker_node.cpp
@@ -6,7 +6,13 @@
 #include <ros/ros.h>
 #include <nav_msgs/Path.h>
 #include <visualization_msgs/MarkerArray.h>
+#include <tf2_ros/transform_listener.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include "color_names/color_names.h"
+#include "tf2/convert.h"
+#include "tf2/exceptions.h"
+#include "tf2_ros/buffer.h"
 
 using namespace std;
 using namespace visualization_msgs;
@@ -15,6 +21,9 @@ using namespace geometry_msgs;
 
 ros::Publisher marker_pub;
 ros::Subscriber path_sub;
+
+std::shared_ptr<tf2_ros::Buffer> tf_buffer_ptr;
+std::shared_ptr<tf2_ros::TransformListener> tf_listener_ptr;
 
 string dToStr(double value, int digit=2)
 {
@@ -29,6 +38,7 @@ string color_name = "black";
 string unit = "m";
 string delimiter = ",";
 int digit = 2;
+string target_frame_id = "";
 
 void paramLoad()
 {
@@ -40,6 +50,7 @@ void paramLoad()
     unit = static_cast<string>(font_data["unit"]);
     delimiter = static_cast<string>(font_data["delimiter"]);
     digit = static_cast<int>(font_data["digit"]);
+    target_frame_id = static_cast<string>(font_data["target_frame_id"]);
   }
 }
 
@@ -82,8 +93,29 @@ void pathCB(const Path::ConstPtr& msg)
   MarkerArray ma_msg;
   for (int i = 0; i < msg->poses.size(); i++)
   {
-    ///@Todo Convert pose by TF?
-    auto m_msg = textMarker(msg->poses[i]);
+    // Convert each pose with target_frame_id
+    Marker m_msg;
+    if (target_frame_id == "")
+    {
+      m_msg = textMarker(msg->poses[i]);
+    }
+    else
+    {
+      geometry_msgs::TransformStamped tf;
+      try
+      {
+        tf = tf_buffer_ptr->lookupTransform(target_frame_id, msg->poses[i].header.frame_id, ros::Time(0));
+        PoseStamped new_poses;
+        tf2::doTransform(msg->poses[i], new_poses, tf);
+        m_msg = textMarker(new_poses);
+      }
+      catch (tf2::TransformException &ex)
+      {
+        ROS_WARN("%s", ex.what());
+        continue;
+      }
+    }
+
     m_msg.id = i;
     ma_msg.markers.push_back(m_msg);
   }
@@ -95,6 +127,9 @@ int main(int argc, char** argv)
 {
   ros::init(argc, argv, "path_to_text_marker_node");
   ros::NodeHandle nh;
+
+  tf_buffer_ptr = make_shared<tf2_ros::Buffer>();
+  tf_listener_ptr = make_shared<tf2_ros::TransformListener>(*tf_buffer_ptr);
 
   marker_pub = nh.advertise<MarkerArray>("path_text_marker", 1, true);
   path_sub = nh.subscribe("path", 1, pathCB);
