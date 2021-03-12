5a5d4e64ced7ec6b5a2e000228b9e24a7331fc54
Shuhei-YOSHIDA <s-yoshida@frontier.hokudai.ac.jp>
2021-03-12 15:17:09 +0900

    worked resolve_path

--- tiny_conversion/include/tiny_conversion/resolve_path.h
+++ tiny_conversion/include/tiny_conversion/resolve_path.h
@@ -6,13 +6,15 @@
 #define INCLUDE_TINY_CONVERSION_RESOLVE_PATH_H
 
 #include <string>
+//#include <boost/filesystem.hpp>
 //#include <filesystem>
 
 namespace tiny_conversion
 {
 
-std::string resolvePathStr(const std::string& path);
+std::string resolvePathUrlStr(const std::string& url);
 //std::filesystem::path resolvePath(const std::string& path);
+//boost::filesystem::path resolvePath(const std::string& path);
 
 }
 
